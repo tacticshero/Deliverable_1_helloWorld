7230ef537b65c05322a0ac4686af50d39252a2f3
rosha <rosha@LAPTOP-78I17KBO>
2022-08-25 16:30:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string stg = "hello world";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter username: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+            
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Age is " + age);
+        }
+    }
+}
+
